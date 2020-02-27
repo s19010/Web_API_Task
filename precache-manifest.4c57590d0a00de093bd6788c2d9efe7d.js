264c1493a447a04f2589becd7c9b39864fd24a1a
s19010 <s19010@std.it-college.ac.jp>
2020-02-27 07:04:18 +0000

    Updates

---
+++ precache-manifest.4c57590d0a00de093bd6788c2d9efe7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d440098c8c992ab7c6137026da38736",
+    "url": "/Web_API_Task/index.html"
+  },
+  {
+    "revision": "c4edf2cf51e30ee46638",
+    "url": "/Web_API_Task/static/css/main.6d88b344.chunk.css"
+  },
+  {
+    "revision": "19edcba6426603e9a2e6",
+    "url": "/Web_API_Task/static/js/2.b6e6c686.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Web_API_Task/static/js/2.b6e6c686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4edf2cf51e30ee46638",
+    "url": "/Web_API_Task/static/js/main.cd43aca8.chunk.js"
+  },
+  {
+    "revision": "319b4862d08e23008579",
+    "url": "/Web_API_Task/static/js/runtime-main.78e31680.js"
+  }
+]);
